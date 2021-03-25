fcbcae8e4c9fdc237e4fb6ea821d884d029b0a95
Wei Tang <wei@that.world>
2021-03-25 07:10:55 +0100

    Add command to import existing keys

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@
 // along with Polkaregistry. If not, see <http://www.gnu.org/licenses/>.
 
 use structopt::StructOpt;
-use sp_core::crypto::{Ss58Codec, Ss58AddressFormat};
+use sp_core::crypto::{Pair, Ss58Codec, Ss58AddressFormat};
 use sp_keystore::SyncCryptoStore;
 use sc_keystore::LocalKeystore;
 use polkaregistry::{SR25519, TweetProof};
@@ -39,6 +39,9 @@ enum Command {
 #[derive(Debug, StructOpt)]
 enum KeystoreCommand {
     Generate,
+    Import {
+        suri: String,
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -60,6 +63,20 @@ fn main() {
         Command::Keystore(KeystoreCommand::Generate) => {
             let public = keystore.sr25519_generate_new(SR25519, None).expect("generating keys failed");
             println!("Generated address: {:?}", public.to_ss58check_with_version(Ss58AddressFormat::PolkadotAccount));
+        }
+        Command::Keystore(KeystoreCommand::Import { suri } ) => {
+            let pair = sp_core::sr25519::Pair::from_string(
+                &suri,
+                None,
+            ).expect("invalid seed");
+
+            keystore.insert_unknown(
+                SR25519,
+                &suri,
+                pair.public().as_ref(),
+            ).expect("insert failed");
+
+            println!("Inserted address: {:?}", pair.public().to_ss58check_with_version(Ss58AddressFormat::PolkadotAccount));
         }
         Command::Sign(SignCommand::Twitter { username, address }) => {
             let proof = TweetProof { username, address };
